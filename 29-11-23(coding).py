9910bb14aff3d8e0dc6dfd53f4b72e88087d3c6f
yuvancoding <112766393+yuvancoding@users.noreply.github.com>
2023-11-29 19:36:16 +0530

    Add files via upload

---
+++ 29-11-23(coding).py
@@ -0,0 +1,38 @@
+class History_in_space():
+    def year_2008(self):
+        print("\n", "Events happend in space 2008", "\n")
+        print("India successfully launched Chandrayaan in 2008 in order to explore the moon.The craft was inserted into lunar orbit in its first attempt. One of the greatest achievement of Chadrayaan was the discoveryof the water molecules in the lunar soil.")
+        
+    def year_2011(self):
+        print("\n", "Events occurred in the field of space in 2011","\n")
+        
+        
+class Histroy_in_medical():
+    def year_2008(self):
+        print("\n" "Events happend in medical 2008", "\n")
+        print("Although scientists have long discovered how malaria is transferred and know how to prevent it, nearly 1 million people die every year from the disease, according to international estimates.Finally, Dec. 8, the first results of a malaria vaccine that shows promise hit international news.")
+        
+    def year_2011(self):
+        print("n", "Events occurred in the field of medical in 2011", "\n")
+        
+        
+class History_in_sports():
+    def year_2008(self):
+        print("\n", "Events happend in sports 2008", "\n")
+        print("In 2008, Abhinav Bindra created history by bagging gold in men's 10m air rifle event at the Beijing Games. His gold remains the country's first and only individual gold medal till date.")
+        
+    def year_2011(self):
+        print("\n", "Events occurred in the field of sports in 2011", "\n")
+        
+
+space = History_in_space()
+space.year_2008()
+space.year_2011()
+
+medical = Histroy_in_medical()
+medical.year_2008()
+medical.year_2011()
+
+sports = History_in_sports()
+sports.year_2008()
+sports.year_2011()
